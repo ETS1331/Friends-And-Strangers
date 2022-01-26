7e84ef573df07e9d145d3f92a6ee210cb09bb561
ETS1331 <45723073+ETS1331@users.noreply.github.com>
2022-01-25 20:56:09 -0800

    Small Changes
    Messed around to make the code better; slightly edited readme, and created connected component checker.

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 Spaghetti Code For PRIMES.
 
 # Usage
-I'm lazy, so for now, install the libraries networkx and mathplotlib, and run the file called "tests".
+I'm lazy, so for now, install required files with cmd open in the folder where this is located and do
+```
+pip install requirements.txt
+```
+and everything else should be somewhat intuitive (see the examples).
 
--- main.py
+++ main.py
@@ -5,31 +5,38 @@ import math
 from itertools import permutations
 class Graph:
     def __init__(self,vertices):
-        self.visual = []
-        self.vertices = vertices #Please let this number be correct
-    def Edge(self, a, b):
-        temp = [a, b]
-        self.visual.append(temp)
+        self.edges = []
+        
+        self.G = nx.Graph()
+        if type(vertices) == list:
+            self.G.add_nodes_from(vertices)
+            self.vertices = vertices
+        else:
+            self.G.add_nodes_from([i for i in range(vertices)])
+            self.vertices = [i for i in range(vertices)]
+    def edge(self, a, b):
+        self.G.add_edge(a,b)
+        self.edges.append([a,b])
     def visualize(self):
-        G = nx.Graph() 
-        G.add_edges_from(self.visual)
         plt.figure(1,figsize=(12,12)) 
-        nx.draw_spring(G,font_size=8)
+        nx.draw_networkx(self.G)
         plt.show()
+    def connected(self):
+        print(nx.number_connected_components(self.G))
 def combine(G,H): #Create the friends and strangers graph based on the graphs
-    if G.vertices != H.vertices: #Force equal number of vertices
+    if len(G.vertices) != len(H.vertices): #Force equal number of vertices
         return None
-    v = G.vertices
-    I = Graph(math.factorial(v))
+    v = len(G.vertices)
     k = permutations([i+1 for i in range(v)])
     k = list(k)
+    I = Graph(k)
     for i in range(len(k)): 
         for a in range(1,v+1):
             for b in range(1,v+1):
-                if  [a,b] in G.visual and [k[i][a-1],k[i][b-1]] in H.visual: #Swap Conditions! 
+                if  [a,b] in G.edges and [k[i][a-1],k[i][b-1]] in H.edges: #Swap Conditions! 
                     tmp = list(k[i])
                     tmp[a-1] = k[i][b-1]#ok maybe I should've just let a,b go from 0 to v-1 instead
                     tmp[b-1] = k[i][a-1] 
                     tmp = tuple(tmp)
-                    I.Edge(k[i],tmp)
-    I.visualize()
+                    I.edge(k[i],tmp)
+    return I
